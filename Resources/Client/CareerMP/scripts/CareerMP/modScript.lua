8a9568709813f88ebca69f11ecda4c9d8d3ee69c
Dudekahedron <49531350+StanleyDudek@users.noreply.github.com>
2026-02-23 13:03:47 -0800

    Delete Resources/Client directory

--- Resources/Client/CareerMP/scripts/CareerMP/modScript.lua
+++
@@ -1,8 +0,0 @@
-load('CareerMP')
-setExtensionUnloadMode('CareerMP', 'manual')
-
-load('/career/mpCareer')
-setExtensionUnloadMode('/career/mpCareer', 'manual')
-
-load('/freeroam/mpBigMapMode')
-setExtensionUnloadMode('/freeroam/mpBigMapMode', 'manual')
