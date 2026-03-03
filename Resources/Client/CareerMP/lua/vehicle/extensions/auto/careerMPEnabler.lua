f31da0ff269ac04d5a39e08a8c8d07caf5af7d76
Dudekahedron <49531350+StanleyDudek@users.noreply.github.com>
2026-03-02 20:20:55 -0800

    part of spawn area physics disable, calls back to GE

--- Resources/Client/CareerMP/lua/vehicle/extensions/auto/careerMPEnabler.lua
+++ Resources/Client/CareerMP/lua/vehicle/extensions/auto/careerMPEnabler.lua
@@ -5,6 +5,12 @@ local function onVehicleReady()
 	obj:queueGameEngineLua("careerMPEnabler.onVehicleReady(" .. obj:getID() .. ") ")
 end
 
+local function callback(id, distance)
+	obj:queueGameEngineLua("careerMPEnabler.callback(" .. id .. ", " .. distance .. ") ")
+end
+
 M.onVehicleReady = onVehicleReady
+
+M.callback = callback
 
 return M
