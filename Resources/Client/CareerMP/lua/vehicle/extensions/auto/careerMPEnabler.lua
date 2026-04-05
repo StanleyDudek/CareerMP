60ee6f30f703a5c1b40bb7919ee44afa92572664
Dudekahedron <49531350+StanleyDudek@users.noreply.github.com>
2026-04-04 20:41:14 -0700

    part condition check and callback

--- Resources/Client/CareerMP/lua/vehicle/extensions/auto/careerMPEnabler.lua
+++ Resources/Client/CareerMP/lua/vehicle/extensions/auto/careerMPEnabler.lua
@@ -8,6 +8,14 @@ local function onVehicleReady()
 	obj:queueGameEngineLua("careerMPEnabler.onVehicleReady(" .. obj:getID() .. ") ")
 end
 
+local function onConditionCheck()
+	if partCondition and partCondition.getConditions() then
+		obj:queueGameEngineLua("careerMPEnabler.onConditionCheckCallback(" .. obj:getID() .. ") ")
+	end
+end
+
+M.onConditionCheck = onConditionCheck
+
 M.onVehicleReady = onVehicleReady
 
 return M
